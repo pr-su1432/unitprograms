447566b05cf76a27e53fc2f329dbc9ef80b32452
[prasanna] <prasanna>
2022-05-22 21:40:47 +0530

    [prasanna]: calculating square root of a number

---
+++ unitprograms/squareroot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unitprograms
+{
+    internal class squareroot
+    {
+        public void square()
+        {
+            string x;
+            double sqrtofx;
+
+            Console.Write("Enter a number: ");
+            x = Console.ReadLine();
+
+            sqrtofx = Math.Sqrt(Convert.ToInt32(x));
+
+            Console.WriteLine("\nSqrt({0}): {1}", x, sqrtofx);
+
+            Console.ReadKey();
+        }
+    }
+}
